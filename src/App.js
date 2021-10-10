8fd7f5571350f0e7700776917749eeeb22cf3899
elik <elik@example.com>
2021-10-10 19:45:12 +0300

    finishWork

--- src/App.js
+++ src/App.js
@@ -4,155 +4,85 @@ import Timeline from './components/Timeline';
 
 function App() {
 
-   
-   let steps = [
-      
-      {
-      id: 1,
-      status: ''
+   const [steps, setSteps] = React.useState([
+      { 
+         id: 1,
+         status: ''
+      },
+      { 
+         id: 2,
+         status: ''
       },
       {
-      id: 2,
-      status: ''
+         id: 3,
+         status: ''
       },
-      {
-      id: 3,
-      status: ''
-      },
-   ]
-
-   
+   ])
+   const [currentStep, setCurrentStep] = React.useState(0)
+   const [underTitle, setUnderTitle] = React.useState(false)
+   const [buttonValue, setButtonValue] = React.useState(false)
    const [stepsLikeDot, setStepsLikeDot] = React.useState(false)
-   const [nextStepStart, setNextStepStart] = React.useState(false)
 
-   const nextMove = () =>{
-      setNextStepStart(!nextStepStart)
 
+   const addStepHandler = () =>{
+      setSteps(
+         [...steps, {
+            id: steps.length + 1,
+            status: ''
+         }]
+      )
    }
 
-   // const addStep = () =>(
-   //    [...steps, 
-   //       {
-   //          id: steps.length + 1,
-   //          status: 'default'
-   //       }]
-   // )
-
-   const addStep = () =>{
-      let newStep = {
-         id: steps.length + 1,
-         status:''
-      }
-      console.log(newStep);
-      steps.push(newStep)
-      console.log(steps);
+   const onUnderHadler = () =>{
+      setUnderTitle(!underTitle)
    }
 
-   console.log(steps);
-   
-   window.onload = () =>{
-
-      const start = document.querySelector('.start')
-      const finish = document.querySelector('.finish')
-      // const toggle = document.querySelector('.toggle')
-      const steps = document.querySelectorAll('.timeline__item')
-      start.addEventListener('click', startStep)
-      finish.addEventListener('click', finishStep)
-      
-      // toggle.addEventListener('click', ()=>{
-      //    document.querySelector('.timeline').classList.toggle('step-center')
-      // })
-      
-      let d = 0
-      let f = 0
-      function startStep(){
-         for(let i = 0; steps.length > i; i++){
-            steps[d].classList.add('active')
-         }
-         d++
-         // steps.forEach(item => item[i].classList.add('active'))
+   const onStartHandler = () =>{
+      setButtonValue(!buttonValue)
+      if(steps[currentStep].status === ''){
+         steps[currentStep].status = 'active'
+      }else if(steps[currentStep].status === 'active'){
+         steps[currentStep].status = 'finish'
+      }else{
+         setCurrentStep(currentStep + 1)
+         steps[currentStep + 1].status = 'active'
       }
-      function finishStep(){
-         for(let i = 0; steps.length > i; i++){
-            steps[f].classList.add('finish')
-         }
-         f++
-         // steps.forEach(item => item[i].classList.add('finish'))
-      }
+      const cloneSteps = [...steps]
+      setSteps(cloneSteps)
    }
-  return (
 
-   <div>
-      <div className={`timeline__wrap ${stepsLikeDot ? 'step-center' : ''}`}>
-         <Timeline steps={steps}/>
-      </div>
-      <div className="timeline-controls">
-         <button 
-            className="start"
-            onClick={nextMove}
-         >start</button>
-         <button 
-            className="toggle"
-            onClick={() => setStepsLikeDot(!stepsLikeDot)}
-         >
-             type
+   return (
+      <div>
+         <div className={`timeline__wrap ${stepsLikeDot ? 'step-center' : ''}`}>
+            <Timeline 
+               steps={steps}
+               underTitle={underTitle}/>
+         </div>
+         <div className="timeline-controls">
+            <button 
+               className="toggle"
+               onClick={() => setStepsLikeDot(!stepsLikeDot)}
+            >
+               Тип
             </button>
-         <button className="finish">finish</button>
-         <button 
-            className="add-step"
-            onClick={()=>addStep()}
-         >add-step</button>
-      </div>
-   </div>
-     
-   //  <div classNameName="modal">
-   //    <form action="post">
-   //       <h1 classNameName="modal__title">Create a new account</h1>
-   //       <Input 
-   //          label="Enter your name"
-   //          errorMessage="Please enter a valid name"
-   //          classNameNameForInput="modal__name-input"
-   //          haveValue={setName}
-   //          isInvalid={isInvalidName}
-   //       />
-   //       <Input 
-   //          label="Mail"
-   //          errorMessage="Please enter a valid email address"
-   //          classNameNameForInput="modal__mail-input"
-   //          haveValue={setMail}
-   //          isInvalid={isInvalidMail}
-   //       />
-   //       <Input 
-   //          label="Password"
-   //          errorMessage="Password must contain at least 6 symbols"
-   //          classNameNameForInput="modal__pass-input"
-   //          type='password'
-   //          haveValue={setPass}
-   //          isInvalid={isInvalidPass}
-   //       />
-   //       <Country 
-   //          choosenCountry={choosenCountry}
-   //          setCountry={setCountry}
-   //          countries={countries}
-   //          isInvalid={isInvalidCountry}
-   //          errorMessage="You must select your country"/>
-   //       <Sex 
-   //          errorMessage='You must select the gender'
-   //          isInvalid={isInvalidSex}
-   //          haveValue={setRadio}/>
-   //       <Accept 
-   //          haveValue={setAccept}
-   //          isInvalid={isInvalidAccept}
-   //          errorMessage="You must accept the policies"
-   //          onChange ={setAccept}/>
-   //       <Button 
-   //          loading={loading}
-   //          onClick={onClickSubmit}
-   //          disabled={name && mail && pass && radio && accept}/>
+            <button 
+               disabled={steps[steps.length - (stepsLikeDot ? 2 : 1)].status === 'finish'}
+               className="start"
+               onClick={onStartHandler}
+            >{buttonValue ? 'Завершить выполнение' : 'Приступить к выполнению'}</button>
             
-   //    </form>
-   // </div>
-  );
+            <button 
+               onClick={onUnderHadler}
+            >
+               Вид заголовков
+            </button>
+            <button 
+               className="add-step"
+               onClick={addStepHandler}
+            >Добавить шаг</button>
+         </div>
+      </div>
+   );
 }
 
 export default App;
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,27 +1,22 @@
 import React from 'react'
 
-function Timeline({ steps, }) {
-
-    const [stepsArr, setStepsArr] = React.useState(null)
-    
+function Timeline({ steps, underTitle}) {
    return (
-    
-    <div className="timeline ">
+    <div className={`timeline ${underTitle ? 'under' : ''}`}>
         {steps && steps.map((item)=>{
                 return(
                     <div 
                         className={` timeline__item ${item.status}`}
-                        key = {item.id}
-                    >
-                        <p 
-                            className="timeline__text"
-                        >
-                            {item.id} step
-                        </p>
-                        <div className="timeline__dot"></div>
-                        <div className="timeline__line">
-                            <div className="timelime__progress"></div>
-                        </div>
+                        key = {item.id}>
+                            <p 
+                                className="timeline__text"
+                            >
+                                {item.id} step
+                            </p>
+                            <div className="timeline__dot"></div>
+                            <div className="timeline__line">
+                                <div className="timelime__progress"></div>
+                            </div>
                     </div>
                 )
             })}
